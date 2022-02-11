d9522e959a18adf0154844be3a138f69ca3223bf
Arnav Singh <97420630+Xrnav@users.noreply.github.com>
2022-02-11 11:59:55 +0530

    Cyborg UserBot

---
+++ cyborgbot/random_strings/devs.py
@@ -0,0 +1,21 @@
+DEVLIST = [
+    "5250449841",
+    "1986676404",
+    
+]
+
+
+HARMFUL = [
+    "SESSION",
+    "HEROKU_API_KEY",
+    "base64",
+    "bash",
+    "get_me()",
+    "phone",
+    "os.system",
+    "sys.stdout",
+    "sys.stderr",
+    "subprocess",
+    "CYBORGBOT_SESSION",
+    "session.save()",
+]
