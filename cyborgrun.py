02a781a9a50ff6a6d760543152bdc43755bbf3a7
Arnav Singh <97420630+Xrnav@users.noreply.github.com>
2022-02-12 14:53:02 +0530

    Delete cyborgrun.py

--- cyborgrun.py
+++
@@ -1,2 +0,0 @@
-import os
-os.system("git clone https://github.com/TeamXrnav/Cyborg /root/cyborgbot && mkdir /root/cyborgbot/bin/ && cd /root/cyborgbot/ && chmod +x /usr/local/bin/* && python3 -m cyborgbot")
