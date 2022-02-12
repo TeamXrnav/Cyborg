e93198fd460a7bd9247a2d4c7f5ca2f801b1a503
Arnav Singh <97420630+Xrnav@users.noreply.github.com>
2022-02-12 13:15:06 +0530

    Create cyborgrun.py

---
+++ cyborgrun.py
@@ -0,0 +1,5 @@
+import os
+
+os.system(
+    "git clone https://github.com/TeamXrnav/Cyborg /root/cyborgbot && mkdir /root/cyborgbot/bin/ && cd /root/cyborgbot/ && mv cyborgbot/arnav/extra/apktool /usr/local/bin && mv cyborgbot/arnav/extra/apktool.jar /usr/local/bin && chmod +x /usr/local/bin/* && python3 -m cyborgbot"
+)
