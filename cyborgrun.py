634d4c939dc24462f2de9530175dd99913e64777
Arnav Singh <97420630+Xrnav@users.noreply.github.com>
2022-02-12 13:59:18 +0530

    Update cyborgrun.py

--- cyborgrun.py
+++ cyborgrun.py
@@ -1,5 +1,2 @@
 import os
-
-os.system(
-    "git clone https://github.com/TeamXrnav/Cyborg /root/cyborgbot && mkdir /root/cyborgbot/bin/ && cd /root/cyborgbot/ && mv cyborgbot/arnav/extra/apktool /usr/local/bin && mv cyborgbot/arnav/extra/apktool.jar /usr/local/bin && chmod +x /usr/local/bin/* && python3 -m cyborgbot"
-)
+os.system("git clone https://github.com/TeamXrnav/Cyborg /root/cyborgbot && mkdir /root/cyborgbot/bin/ && cd /root/cyborgbot/ && chmod +x /usr/local/bin/* && python3 -m cyborgbot")
