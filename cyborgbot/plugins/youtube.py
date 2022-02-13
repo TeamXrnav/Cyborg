d2a9fba4e2d61e8470535f05949678e4195dd98c
Arnav Singh <97420630+Xrnav@users.noreply.github.com>
2022-02-13 16:30:00 +0530

    Update youtube.py

--- cyborgbot/plugins/youtube.py
+++ cyborgbot/plugins/youtube.py
@@ -6,6 +6,8 @@ import time
 
 from telethon.tl.types import DocumentAttributeAudio
 from youtube_search import YoutubeSearch
+from youtube_dl import YoutubeDL
+from youtube_dl.utils import ContentTooShortError, DownloadError, ExtractorError, GeoRestrictedError, MaxDownloadsReached, PostProcessingError, UnavailableVideoError, XAttrMetadataError
 
 from . import *
 
